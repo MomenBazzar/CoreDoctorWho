20a3e72a0a2f42506aa3343b5e9f42d7bde1383e
Momen Bazzar <momen.b2001@gmail.com>
2022-06-29 20:23:26 +0300

    applying first 5 requirements

---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoCoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+              .UseSqlServer(
+                "Data Source=MomenLab\\SQLEXPRESS;initial catalog=DoctorWhoCore;persist security info=True;Integrated Security=SSPI;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ DoctorWho.Db/Entities/Episode.cs
@@ -0,0 +1,19 @@
+﻿namespace DoctorWho.Db.Entities
+{
+    class Episode
+    {
+        public int EpisodeId { get; set; }
+        public int SeriesNumber { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string EpisodeType { get; set; }
+        public string Title { get; set; }
+        public DateTime EpisodeDate { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        public string Notes { get; set; }
+        public List<EpisodeCompanion> EpisodeCompanions { get; set; }
+        public List<EpisodeEnemy> EpisodeEnemies { get; set; }
+    }
+}
